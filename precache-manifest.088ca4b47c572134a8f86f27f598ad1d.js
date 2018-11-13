336f8ab723d820000c468153843f02dd56842306
Jakub Dobrowolski <jakub.dobrowolski@besidethepark.com>
2018-11-13 16:25:10 +0100

    Updates

---
+++ precache-manifest.088ca4b47c572134a8f86f27f598ad1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7880bb734007795c8ea4",
+    "url": "/calc-react/static/js/runtime~main.7880bb73.js"
+  },
+  {
+    "revision": "23f01765ef9db472fcc8",
+    "url": "/calc-react/static/js/main.23f01765.chunk.js"
+  },
+  {
+    "revision": "a77b22082848be9d9cc9",
+    "url": "/calc-react/static/js/1.a77b2208.chunk.js"
+  },
+  {
+    "revision": "23f01765ef9db472fcc8",
+    "url": "/calc-react/static/css/main.4ab7719e.chunk.css"
+  },
+  {
+    "revision": "e2b2d1f885511647df0a03994d3924e7",
+    "url": "/calc-react/index.html"
+  }
+];
